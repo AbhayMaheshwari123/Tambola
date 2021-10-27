ca04405496e1b1d0bac5aa3ad0339a67748806cb
AbhayMaheshwari123 <43092362+AbhayMaheshwari123@users.noreply.github.com>
2021-10-27 17:16:33 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Board from './components/board';
+
+const App:React.FC=()=>{
+  return (
+    <div className="App">
+      <Board/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/board.tsx
@@ -0,0 +1,11 @@
+import { Grid } from "@mui/material"
+
+const component:React.FC=()=>{
+    return (<div>
+        <Grid container>
+
+        </Grid>
+    </div>)
+}
+
+export default component
